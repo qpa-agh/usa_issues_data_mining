b39ef688b8001c8bb9daf156c8e03dee0dd39a68
HulekJakub <jakub.hulek01@gmail.com>
2024-10-11 17:43:10 +0200

    scrapping wrappers and inplementation for first representative

--- utils/scraping_utils.py
+++ utils/scraping_utils.py
@@ -1,8 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
+
+session = requests.Session()
 def get_html(url):
-    response = requests.get(url)
+    response = session.get(url)
     if response.ok:
         return BeautifulSoup(response.content, features="lxml")
     return None
