6abd7769b8f09abdd450ed77720ded5e991e1162
paulinagacek <paulina.gacek.pl@gmail.com>
2024-11-23 09:40:08 +0100

    keyphrases

--- key_phrase_extraction/KeyPhraseExtractor.py
+++ key_phrase_extraction/KeyPhraseExtractor.py
@@ -7,16 +7,16 @@ import numpy as np
 
 
 class KeyPhraseExtractor(BaseExtractor):
-    def __init__(self, model: str = "all-MiniLM-L6-v2"):
+    def __init__(self, model: str = "all-MiniLM-L6-v2", top_k: int = 10):
         BaseExtractor.__init__(self)
         self.model = SBERTEmbedder(model)
         self.probs = dict()
-        self.prob_file = 'nr_of_words_in_phrase_prob.pkl'
-        with open(self.prob_file, 'rb') as f:
+        self.prob_file = "nr_of_words_in_phrase_prob.pkl"
+        self.top_k = top_k
+        with open(self.prob_file, "rb") as f:
             self.probs = pickle.load(f)
 
-
-    def extractKeyWords(self, doc: str, top_k:int):
+    def extractKeyWords(self, doc: str):
         sentence_list = self.split_into_sentences(doc)
         phrases = self.generate_candidate_keywords(sentence_list)
         phrases = self.remove_duplicates(phrases)
@@ -27,14 +27,14 @@ class KeyPhraseExtractor(BaseExtractor):
         similarity = self.transform_by_word_count(similarity, phrases)
         keywords = [
             (phrases[index], round(float(similarity[index]), 4))
-            for index in similarity.argsort()[-top_k:]
+            for index in similarity.argsort()[-self.top_k :]
         ][::-1]
-        return list(map(lambda x:x[0], keywords))
-    
+        return list(map(lambda x: x[0], keywords))
+
     def generate_candidate_keywords(self, sentence_list):
         phrase_list = []
         for s in sentence_list:
-            tmp = re.sub(self.stop_words_pattern, '|', s.strip().lower())
+            tmp = re.sub(self.stop_words_pattern, "|", s.strip().lower())
             phrases = tmp.split("|")
             for phrase in phrases:
                 phrase = phrase.strip()
@@ -46,18 +46,18 @@ class KeyPhraseExtractor(BaseExtractor):
         filtered_phrases = []
         stemmed_phrases = set()
         for phrase in phrases:
-            stemmed = ' '.join(self.porter.stem(word) for word in phrase.split())
+            stemmed = " ".join(self.porter.stem(word) for word in phrase.split())
             if stemmed not in stemmed_phrases:
                 stemmed_phrases.add(stemmed)
                 filtered_phrases.append(phrase)
         return filtered_phrases
-    
+
     def transform_by_word_count(self, similarity, phrases: List[str]):
         for idx in range(len(similarity)):
             count = len(phrases[idx].split(" "))
             count_probability = self.probs.get(count, 0.0001)
-            if count_probability < 0.01 or similarity[idx]< 0:
-                similarity[idx]=0
+            if count_probability < 0.01 or similarity[idx] < 0:
+                similarity[idx] = 0
             else:
-                similarity[idx]*=np.log(count_probability*100+15)
+                similarity[idx] *= np.log(count_probability * 100 + 15)
         return similarity
